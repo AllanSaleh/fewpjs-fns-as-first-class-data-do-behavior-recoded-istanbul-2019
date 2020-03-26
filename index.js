a25bf41407fd49bb4321bbb59831a137d231a2ea
Allan Saleh <allan@re-coded.com>
2020-03-26 07:07:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  let hrMin = time.split(':');
+  let hr = parseInt(hrMin[0]);
+  let min = parseInt(hrMin[1]);
+  if(hr<12 && min==01){
+    return "Good Morning";
+  }else if(hr>12 && min<17){
+    return "Good Afternoon";
+  }else{
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(contents){
+  document.getElementById(greeting).content = content;
+}
